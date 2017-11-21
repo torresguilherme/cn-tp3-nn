9bf16dfa7caa18c6b69a9604c87da36ac871d896
Guilherme Torres <guilhermetorres97@gmail.com>
2017-11-21 18:09:32 -0200

    parametros OK

--- src/main.py
+++ src/main.py
@@ -2,6 +2,11 @@ from libs.io import Data
 import sys
 
 TRAIN_FILE = sys.argv[1]
+WEIGHT_INIT = sys.argv[2]
+ACTIVATION_FUNC = sys.argv[3]
+HIDDEN_LAYER_NUMBER = int(sys.argv[4])
+EPOCHS = int(sys.argv[5])
+BATCH_SIZE = int(sys.argv[6])
 
 def main():
     train_data = Data(TRAIN_FILE);
